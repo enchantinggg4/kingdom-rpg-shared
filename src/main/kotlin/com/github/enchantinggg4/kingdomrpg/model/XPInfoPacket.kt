19755e919b58e675343519f42e983b26fe9bde51
Yuri Stepin <enchantinggg4@gmail.com>
2022-01-17 20:51:04 +0300

    Fix missing annotation

--- src/main/kotlin/com/github/enchantinggg4/kingdomrpg/model/XPInfoPacket.kt
+++ src/main/kotlin/com/github/enchantinggg4/kingdomrpg/model/XPInfoPacket.kt
@@ -1,5 +1,8 @@
 package com.github.enchantinggg4.kingdomrpg.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class XPInfoPacket(
     val paths: List<SkillPathDTO>,
     val skills: List<SkillLevelDTO>
